4cdd184d2eead5ba910201c71fdae44da3f87cb7
Memilihdiam <sheep7928@gmail.com>
2025-07-07 19:03:09 +0000

    menambah fitur baru

--- app.py
+++ app.py
@@ -1,15 +1,12 @@
-from flask import Flask, render_template, jsonify, url_for, redirect, request
-import os
+from flask import Flask, render_template, response, request
+import time
+import threading
 
 app = Flask(__name__, template_folder = "app/templates")
 
 @app.route('/')
 def index():
     return render_template("index.html")
-
-@app.route('/cam', methods=['POST'])
-def TakePhoto():
-    return url_for('index')
 
 if __name__ == "__main__":
     app.run(debug=False, port = 4000)
